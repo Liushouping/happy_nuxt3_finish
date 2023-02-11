18b2995397a7f41ab6c99f4a323d5cf6447b9d65
liushouping <kose146352@gmail.com>
2023-02-11 20:54:55 +0800

    fix:finish

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="w-[90%] flex flex-row py-12">
 		<div class="w-full lg:w-1/2 flex flex-col">
-			<h5 class="text-[#001858] text-[24px] font-Anton tracking-[2px]">Footer</h5>
+			<h5 class="text-[#001858] text-[24px] font-Anton tracking-[2px]">Footer A</h5>
 			<p class="text-[#001858] pt-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
 		</div>
 		<div class="w-full lg:w-1/2">
